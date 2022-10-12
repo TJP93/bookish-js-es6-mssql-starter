32872ee24f1e5f5bd10d89e724fc06259d5f0936
Tom Porter <tom.port@yahoo.co.uk>
2022-10-12 14:21:09 +0100

    for loop added to gather correct book by ID

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -11,42 +11,78 @@ class BookController {
     getAllBooks(request, response) {
         console.log( "request for all books" + request.url );
         const mock = [{
-            "id": id,
+            "id": 1,
             "title": "mock",
             "author": "lewis"
         },
         {
-            "id": id,
+            "id": 2,
             "title": "mock2",
             "author": "martin"
         },
         {
-            "id": id,
+            "id": 3,
             "title": "mock3",
             "author": "dickens"
         },
         {
-            "id": id,
+            "id": 4,
             "title": "mock4",
             "author": "shakespeare"
         },
         {
-            "id": id,
+            "id": 5,
             "title": "mock5",
             "author": "tolkien"
         }
-    ];                                                                                                     // mock object make array for plausible response.
-        response.status(200).send(JSON.stringify(mock) );
+    ]; 
+    let responseObject = {"result" : mock}                                                                                                    // mock object make array for plausible response.
+        response.status(200).send(JSON.stringify(responseObject, null, 2) );
     }
 
     getBook(request, response) {
         const id = request.params.id;
+        const mock = [{
+            "id": 1,
+            "title": "mock",
+            "author": "lewis"
+        },
+        {
+            "id": 2,
+            "title": "mock2",
+            "author": "martin"
+        },
+        {
+            "id": 3,
+            "title": "mock3",
+            "author": "dickens"
+        },
+        {
+            "id": 4,
+            "title": "mock4",
+            "author": "shakespeare"
+        },
+        {
+            "id": 5,
+            "title": "mock5",
+            "author": "tolkien"
+        }
+    ];
         console.log( "request for book " + id );
         if ( id == 0 ){
             throw ( "bad id");
         }
-        const mock = {"id": id, "title" : "mock", "author": "lewis"};  // mock object. Work out how to return some different books rather than just lewis, array? if id > x etc?
-        response.status(200).send(JSON.stringify(mock) );
+        for (let i = 0; i <mock.length; i++) {
+            if (mock[i].id == id) {
+                response.status(200).send(JSON.stringify(mock[i]) );
+
+            }
+        }
+
+        
+
+       //  const mock = {"id": 1, "title" : "mock", "author": "lewis"};  // mock object. Work out how to return some different books rather than just lewis, array? if id > x etc? For loop?
+       // response.status(200).send(JSON.stringify(mock) );
     }
 }
 
