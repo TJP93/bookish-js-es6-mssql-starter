00686c701cb08332d8c66c4c7f01037803b804e5
Tom Porter <tom.port@yahoo.co.uk>
2022-10-12 12:25:10 +0100

    Mock array created for getAllBooks

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -10,7 +10,33 @@ class BookController {
 
     getAllBooks(request, response) {
         console.log( "request for all books" + request.url );
-        response.status(500).send({"message" : "please try later" } );
+        const mock = [{
+            "id": id,
+            "title": "mock",
+            "author": "lewis"
+        },
+        {
+            "id": id,
+            "title": "mock2",
+            "author": "martin"
+        },
+        {
+            "id": id,
+            "title": "mock3",
+            "author": "dickens"
+        },
+        {
+            "id": id,
+            "title": "mock4",
+            "author": "shakespeare"
+        },
+        {
+            "id": id,
+            "title": "mock5",
+            "author": "tolkien"
+        }
+    ];                                                                                                     // mock object make array for plausible response.
+        response.status(200).send(JSON.stringify(mock) );
     }
 
     getBook(request, response) {
@@ -19,7 +45,7 @@ class BookController {
         if ( id == 0 ){
             throw ( "bad id");
         }
-        const mock = {"id": id, "title" : "mock", "author": "lewis"};  // mock object.
+        const mock = {"id": id, "title" : "mock", "author": "lewis"};  // mock object. Work out how to return some different books rather than just lewis, array? if id > x etc?
         response.status(200).send(JSON.stringify(mock) );
     }
 }
